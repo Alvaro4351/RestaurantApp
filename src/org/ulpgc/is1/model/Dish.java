39835c2c98db84a7e5927afa4e34134cbb2bca6e
Álvaro <alvarojrr4@gmail.com>
2023-10-20 12:14:50 +0100

    prueba alvaro3

--- src/org/ulpgc/is1/model/Dish.java
+++ src/org/ulpgc/is1/model/Dish.java
@@ -1,4 +1,39 @@
 package org.ulpgc.is1.model;
-
+//Generación de la clase
 public class Dish {
+    private String name;
+    priva
+    te String description;
+    private int price;
+
+    //Generación del constructor
+    public Dish(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+    //Funciones get() y set()
+    private String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private String getDescription() {
+        return description;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    private int getPrice() {
+        return price;
+    }
+
+    private void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/org/ulpgc/is1/model/OrderItem.java
+++ src/org/ulpgc/is1/model/OrderItem.java
@@ -1,4 +1,30 @@
 package org.ulpgc.is1.model;
-
+//Generamos la clase
 public class OrderItem {
+    private int quantity;
+    private Dish plato;
+
+    //Generamos el constructor
+    public OrderItem(int quantity, Dish plato) {
+        this.quantity = quantity;
+        this.plato = plato;
+    }
+
+    //Funciones get() y set()
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Dish getPlato() {
+        return plato;
+    }
+
+    public void setPlato(Dish plato) {
+        this.plato = plato;
+    }
 }
